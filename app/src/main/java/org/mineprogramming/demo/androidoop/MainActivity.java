7664a91b80a759921a743563acb3f1caa5eea8c0
IchZerowan <Ih01@i.ua>
2019-07-24 19:09:37 +0300

    Created new Android project

---
+++ app/src/main/java/org/mineprogramming/demo/androidoop/MainActivity.java
@@ -0,0 +1,15 @@
+package org.mineprogramming.demo.androidoop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
